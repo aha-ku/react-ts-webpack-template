555b19082aaf3f8f9a3eb50d968b390094ce66cb
zjfcool <2568696196@qq.com>
2022-01-15 16:47:20 +0800

    init

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -2,6 +2,7 @@ const { merge } = require('webpack-merge')
 const commonConfig = require('./webpack.common');
 module.exports = merge(commonConfig, {
     mode: "development",
+    devtool: "source-map",
     devServer: {
     }
 })
--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -5,11 +5,18 @@ const webpackCommon = require('./webpack.common')
 module.exports = merge(webpackCommon, {
     mode: "production",
     optimization: {
+        minimize: true,
         minimizer: [
             new CssMinimizerWebpackPlugin(),
             new ESBuildMinifyPlugin({
-                target: "es2015"
+                target: "es2015",
+                minifyWhitespace: true,
+                // css: true
             })
-        ]
+        ],
+        splitChunks: {
+            chunks: 'all',
+            minSize: 4 * 1024
+        }
     }
 })
